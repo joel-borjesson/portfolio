eb78bd45b09b48408c6a13ec22d4bd54fbb6f765
joel-borjesson <semilogical@pm.me>
2023-08-25 13:49:19 -0700

    adjust night hamburger menu colors

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const nightswitch = document.querySelectorAll(".bios > div");
 const menuswitch = document.querySelector("nav ul");
+const hamburgerswitch = document.querySelector("#hamburger-menu")
 function nightToggle() {
     for (var i=0; i<nightswitch.length; i++){
         nightswitch[i].classList.toggle("night-mode-bios");
     };    
     document.body.classList.toggle("night-mode");
     menuswitch.classList.toggle("night-mode");
+    hamburgerswitch.classList.toggle("night-hamburger");
 }
